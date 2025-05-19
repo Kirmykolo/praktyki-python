a85a9fb7a0d7ea980a9a00fcf5f18ed1fe1437e0
Kirmykolo <jakubpiotrowski6637@gmail.com>
2025-05-19 16:37:49 +0200

    zmieniony kod przez chat gpt

--- main.py
+++ main.py
@@ -1,12 +1,22 @@
 from bs4 import BeautifulSoup
 from requests import get
+import sys
+
+sys.stdout.reconfigure(encoding='utf-8')
 
 URL = 'https://www.onet.pl/'
 
 page = get(URL)
+soup = BeautifulSoup(page.content, 'html.parser')
 
-artykuly = BeautifulSoup(page.content, 'html.parser')
+# Pobranie wszystkich artykułów
+artykuly = soup.find_all("a", class_='Common_linkCardLink__cN081 SectionLink_linkStyle__xYsXd')
 
-for artykul in artykuly.find_all('h3'):
-    print(artykul.text.strip())
-    break
+# Filtrowanie tylko tych, które należą do sekcji "wiadomości"
+for artykul in artykuly:
+    data_gtm = artykul.get("data-gtm", "")  # Pobieramy atrybut "data-gtm"
+    
+    if "news_" in data_gtm:  # Sprawdzamy, czy artykuł należy do sekcji "wiadomości"
+        naglowek = artykul.find('h3', class_='TitleWrapper_titleWrapper__7S_PA')
+        if naglowek:
+            print(naglowek.text.strip())
